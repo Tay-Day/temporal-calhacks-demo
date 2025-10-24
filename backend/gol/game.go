abfb3dea0b76ee2352503f3e86e1637afc82254a
Tayler <tayler@usestyle.ai>
2025-10-24 12:03:31 -0700

    fix: remove splatter

--- backend/gol/game.go
+++ backend/gol/game.go
@@ -60,24 +60,6 @@ func GameOfLife(ctx workflow.Context, input GameOfLifeInput) (err error) {
 			log.Fatalf("Error next generation and sending state: %v", err)
 		}
 	})
-	selector.AddReceive(splatterChannel, func(c workflow.ReceiveChannel, more bool) {
-		var signal SplatterSignal
-		c.Receive(ctx, &signal)
-
-		err = DoActivity(ctx, AmInstance.Splatter, SplatterInput{
-			Row:    signal.X,
-			Col:    signal.Y,
-			Radius: signal.Size,
-		})
-		if err != nil {
-			log.Fatalf("Error splattering board: %v", err)
-		}
-		NextGenerationAndSendState(ctx, state)
-		if err != nil {
-			log.Fatalf("Error next generation and sending state: %v", err)
-		}
-	})
-
 	// Steps through the generations
 	for Steps < input.MaxSteps {
 
